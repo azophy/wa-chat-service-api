8be336483fd1b0bd7e62fb6f2e2ea6aa6b293acf
Abdurrahman Shofy Adianto <azophy@gmail.com>
2020-12-03 13:20:51 +0700

    implement basic /status & /logout endpoint

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ import {
     WAConnection,
     ReconnectMode,
     MessageType,
+    DisconnectReason,
 } from '@adiwajshing/baileys'
 import * as QR from 'qrcode-terminal'
 import * as fs from 'fs'
+
+var global_conn
 
 async function connectToWhatsApp () {
     const conn = new WAConnection() 
@@ -36,8 +39,53 @@ async function connectToWhatsApp () {
     const id = '6282142425544@s.whatsapp.net' // the WhatsApp ID 
     // send a simple text!
     conn.sendMessage (id, 'oh hello there', MessageType.text)
+
+    return conn
 }
 
-// run in main file
-connectToWhatsApp ()
-.catch (err => console.log("unexpected error: " + err) ) // catch any errors
+// =========== REST API SECTION ==========
+import express from 'express'
+
+const PORT = 3000
+
+const app = express()
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+  res.status(200).json({ status: 'ok' })
+})
+
+app.get("/status", (req, res) => {
+  var connected = false,
+      username = null
+  try {
+    username = global_conn.user.name + ' (' + global_conn.user.jid + ')'
+  } catch (e) {
+    connected = false 
+  } 
+
+  res.status(200).json({ 
+      connected: connected,
+      current_user: username,
+  })
+})
+
+app.get("/logout", (req, res) => {
+   if (global_conn != undefined) {
+      global_conn.closeInternal(DisconnectReason.intentional)
+   }
+
+  res.status(200).json({ 
+    success: true,
+  })
+})
+
+app.listen(PORT, () => {
+    console.log('Server started')
+
+    // run whatsapp client
+    connectToWhatsApp ()
+    .then( conn => global_conn = conn )
+    .catch (err => console.log("unexpected error: " + err) ) // catch any errors
+})
